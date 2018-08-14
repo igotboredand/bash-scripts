486d8b76a49ef78a95fbe74e2178f40bdfe59e3f
igotboredand <46539368+igotboredand@users.noreply.github.com>
2018-08-14 06:02:47 -0400

    modified:   openvpn_config.sh
    added check for openvpn client configuration in local directory, and a call to copy it from its default location if it doesn't exist.

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # A collection of bash scripts that've saved me time. 
 
-### Structure
+### Structure:
 - Any folder that writes or requires files to be in the current directory has it's own folder.
 - Any script that requires no files is contained in the scripts folder, with the exception of the update_readme script in this current directory. 
 
 
-#### Scripts:
-- htaccess-cloudflare: creates an .htaccess file that locks down a web server to cloudflares resolvers, in case you don't have the ability to deny outside traffic at a lower level
+### Scripts:
+- htaccess-cloudflare: creates an .htaccess file that locks down a web server to cloudflares resolvers, in case you don't have the ability to deny outside traffic at a lower level.
 - openvpn_config: creates a universal ovpn client configuration.
--- openvpn_config/openvpn_config.sh
+++ openvpn_config/openvpn_config.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
-# This script creates a universal ovpn file for your openvpn client. 
-# You will need the necessary files below in your current directory for this to work. 
-# Necessary files: <client-name>.crt, <client-name>.key, ca.crt, ta.key
-
 
 # Usage: $ openvpn_config.sh <client-name>
 
+# This script creates a universal ovpn file for your openvpn client. 
+# You will need the necessary files below in your current directory for this to work. 
+# Necessary files: client.conf, <client-name>.crt, <client-name>.key, ca.crt, ta.key
 
-cat ./base.conf \
-    <(echo -e '<ca>') \
-    ./ca.crt \
-    <(echo -e '</ca>\n<cert>') \
-    ./${1}.crt \
-    <(echo -e '</cert>\n<key>') \
-    ./${1}.key \
-    <(echo -e '</key>\n<tls-auth>') \
-    ./ta.key \
-    <(echo -e '</tls-auth>') \
-    > ./${1}.ovpn
+
+# if client.conf doesn't exist in the current directory; then copy the config from its default location (on ubuntu / debian. YMMV.)
+if [ ! -f ./client.conf ] && [ -f /usr/share/doc/openvpn/examples/sample-config-files/client.conf ]; then
+    cp /usr/share/doc/openvpn/examples/sample-config-files/client.conf ./base.conf
+else
+    echo "You need client.conf in the current directory, or the openvpn sample config in its default location."
+    exit 1 
+fi
+
+cat ./client.conf \
+<(echo -e '<ca>') \
+./ca.crt \
+<(echo -e '</ca>\n<cert>') \
+./${1}.crt \
+<(echo -e '</cert>\n<key>') \
+./${1}.key \
+<(echo -e '</key>\n<tls-auth>') \
+./ta.key \
+<(echo -e '</tls-auth>') \
+> ./${1}.ovpn
+
+
