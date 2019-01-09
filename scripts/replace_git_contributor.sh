076887879cd8b76afa7182d3b02cddba545af569
igotboredand <46539368+igotboredand@users.noreply.github.com>
2019-01-09 12:03:57 -0800

    added git contributor script

---
+++ scripts/replace_git_contributor.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Switch git emails or usernames? This script will update your old commits with a new email / username. 
+
+git filter-branch --env-filter '
+
+OLD_EMAIL="your-old-email@example.com"
+CORRECT_NAME="Correct-Name"
+CORRECT_EMAIL="your-correct-email@example.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
